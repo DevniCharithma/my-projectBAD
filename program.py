83743646dbdc3967ce933d3197acd502ed7a25d3
thennakoontmip <113238732+thennakoontmip@users.noreply.github.com>
2022-09-10 20:56:21 +0530

    Update program.py

--- program.py
+++ program.py
@@ -2,5 +2,5 @@ from student import Student
 from subject import Subject
 
 sInfo = Student('196090E', 'Tim Southee', 'present')
-mInfo = Subject('Data wrangling', '95')
+mInfo = Subject('Data wrangling', 'A+')
 print(sInfo, "Marks for Semester 04 = ", mInfo)
