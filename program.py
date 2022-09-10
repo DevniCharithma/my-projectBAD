c907f0bba877a0596ffde271f6bbc26b1a72ee6c
Devni <devnicharithma99@gmail.com>
2022-09-10 20:15:50 +0530

    Classes imported to program.py

--- program.py
+++ program.py
@@ -1,3 +1,6 @@
+from student import Student
+from subject import Subject
+
 sInfo = Student('196090E', 'Tim Southee', 'present')
 mInfo = Subject('Data wrangling', '95')
 print(sInfo, "Marks for Semester 04 = ", mInfo)
