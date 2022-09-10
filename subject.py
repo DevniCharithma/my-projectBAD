e7b9a54deee67bfa52be84c1bd99b1a72d33ee6f
Devni <devnicharithma99@gmail.com>
2022-09-10 20:14:32 +0530

    Code updated

--- subject.py
+++ subject.py
@@ -2,3 +2,6 @@ class Subject:
     def __init__(self, module, marks):
         self.module = module
         self.marks = marks
+
+    def __str__(self):
+        return self.module + ' ' + self.marks
