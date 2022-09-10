83d8431c8b628904eec2e31bfbfe9f13fa1093bd
Devni <devnicharithma99@gmail.com>
2022-09-10 20:06:37 +0530

    Code updated

--- program.py
+++ program.py
@@ -1,3 +1,3 @@
-sInfo = Student('196090E', 'A.B. Pereira', 'present')
-mInfo = Subject('BAS', '95')
+sInfo = Student('196090E', 'Tim Southee', 'present')
+mInfo = Subject('Data wrangling', '95')
 print(sInfo, "Marks for Semester 04 = ", mInfo)
